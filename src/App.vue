a44ea7e7126af0e2937922cd22998ad43a5b0a85
tackeyy <mailto.takita.yusuke@gmail.com>
2017-06-18 20:19:49 +0900

    Add creating todo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="createTodo"></create-todo>
   </div>
 </template>
 
 <script>
+import sweetalert from 'sweetalert'
 import TodoList from './components/TodoList'
+import CreateTodo from './components/CreateTodo'
 
 export default {
   name: 'app',
   components: {
     TodoList,
+    CreateTodo
   },
   data() {
     return {
@@ -33,6 +37,12 @@ export default {
       }],
     }
   },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo)
+      sweetalert('Success!', 'To-Do created!', 'success')
+    }
+  }
 }
 </script>
 
---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class='ui basic content center aligned segment'>
+    <button class='ui basic button icon' v-on:click="openForm" v-show="!isCreating">
+      <i class='plus icon'></i>
+    </button>
+    <div class='ui centered card' v-show="isCreating">
+      <div class='content'>
+        <div class='ui form'>
+          <div class='field'>
+            <label>Title</label>
+            <input v-model="titleText" type='text'>
+          </div>
+          <div class='field'>
+            <label>Project</label>
+            <input v-model="projectText" type='text'>
+          </div>
+          <div class='ui two button attached buttons'>
+            <button class='ui basic red button' v-on:click="closeForm">
+              Cancel
+            </button>
+            <button class='ui basic blue button' v-on:click="sendForm">
+              Create
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titleText: '',
+      projectText: '',
+      isCreating: false,
+    }
+  },
+  methods: {
+    openForm() {
+      this.isCreating = true
+    },
+    closeForm() {
+      this.isCreating = false
+    },
+    sendForm() {
+      this.isCreating = false
+
+      if (this.titleText.length === 0 || this.projectText.length === 0) return
+
+      const title = this.titleText
+      const project = this.projectText
+      this.$emit('create-todo', {
+        title,
+        project,
+        done: false,
+      })
+      this.titleText = ''
+      this.projectText = ''
+      this.isCreating = false
+    },
+  },
+}
+</script>

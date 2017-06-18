c8c05328968a4a1cb23717cef3b6a4125d8f32ec
tackeyy <mailto.takita.yusuke@gmail.com>
2017-06-18 18:04:21 +0900

    Editing a Todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,33 +2,18 @@
   <div>
     <p class="tasks">完了したタスク: {{ todos.filter(todo => { return todo.done === true }).length }}</p>
     <p class="tasks">未完了のタスク: {{ todos.filter(todo => { return todo.done === false }).length}}</p>
-    <div class='ui centered card' v-for="todo in todos">
-      <div class='content'>
-        <div class='header'>
-          {{ todo.title }}
-        </div>
-        <div class='meta'>
-          {{ todo.project }}
-        </div>
-        <div class='extra content'>
-          <span class='right floated edit icon'>
-            <i class='edit icon'></i>
-          </span>
-        </div>
-        <div class='ui bottom attached green basic button' v-show="todo.done">
-          Completed
-        </div>
-        <div class='ui bottom attached red basic button' v-show="!todo.done">
-          Complete
-        </div>
-      </div>
-    </div>
+    <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
   </div>
 </template>
 
 <script>
+import Todo from './Todo'
+
 export default {
-  props: ['todos']
+  props: ['todos'],
+  components: {
+    Todo,
+  }
 }
 </script>
 
